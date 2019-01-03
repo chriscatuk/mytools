c8689b2f98506bd6b5cad9f5ba2472f5a7de3f98
Christian G <32960017+chriscatuk@users.noreply.github.com>
2019-01-03 09:48:36 +0000

    list-asg-ips.sh

---
+++ aws/list-asg-ips.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# list all instances of the ASG this instance is from
+# IPs comma separated
+
+myid=`curl --silent http://169.254.169.254/latest/meta-data/instance-id 2>&1`
+
+instanceids=`aws autoscaling describe-auto-scaling-instances \
+--output text --region eu-west-1 \
+--instance-ids $myid \
+--query "AutoScalingInstances[*].AutoScalingGroupName" \
+| aws autoscaling describe-auto-scaling-instances --region eu-west-1 --output text \
+--query "AutoScalingInstances[?AutoScalingGroupName=='splktest'].[InstanceId]"`
+
+ipaddresses=`aws ec2 describe-instances --output text --region eu-west-1 \
+--query "Reservations[*].Instances[*].[PrivateIpAddress]" --instance-id $instanceids`
+
+# List of IP
+# echo $ipaddresses
+
+# List of IP, comma separated
+output=""
+for ipaddress in $ipaddresses
+do
+    if [ -z "$output" ]
+    then
+        output="${ipaddress}"
+    else
+        output="${output},${ipaddress}"
+    fi
+done
+echo $output
