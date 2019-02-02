666dafc5a6d84278a3906556bdc7166106860bbd
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2019-02-02 16:42:40 +0000

    error management

--- aws/list-asg-ips.sh
+++ aws/list-asg-ips.sh
@@ -2,39 +2,58 @@
 # list all instances in Service of the ASG this instance is from
 # IPs comma separated
 
-# Check if required commands are installed
-for item in logger \
-            jq \
-            aws \
-            curl
-  do
-    command -v $Item >/dev/null 2>&1 || \
-        { logger -s "DNS Check requires {$item} but it's not installed. Aborting."; exit 1; }
-  done
+# Pre Req Check
+    # Check if required commands are installed
+    for item in logger \
+                jq \
+                aws \
+                curl
+    do
+        command -v $Item >/dev/null 2>&1 || \
+            { cjg_showerror -s "requires {$item} but it's not installed. Aborting."; exit 1; }
+    done
 
-read instanceID region <<< $(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | \
-    jq -c -r '.instanceId, .region')
+# Output the message to standard error as well as to the system log.
+# Start with "Error DNS Check:"
+function cjg_showerror {
+    logger -s "Error DNS Check: $1" -p local0.warn;
+}
 
-echo "the instance ID is $instanceID, in region $region"
+function get_ips_this_asg {
+    read instanceID region <<< $(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | \
+        jq --compact-output --raw-output --exit-status '.instanceId, .region') || \
+            { cjg_showerror "Can't recover Meta-Data Instance ID and Region. Aborting"; exit 1; }
 
-asgName=$(aws autoscaling describe-auto-scaling-instances \
-    --output json --region $region \
-    --instance-ids $myInstanceID \
-    --query "AutoScalingInstances[0]" | \
-    jq -c -r '.AutoScalingGroupName')
+    #echo "the instance ID is $instanceID, in region $region"
 
-echo "the asg Name is $asgName"
+    asgName=$(aws autoscaling describe-auto-scaling-instances \
+        --output json --region $region \
+        --instance-ids $myInstanceID \
+        --query "AutoScalingInstances[0]" | \
+        jq -rce '.AutoScalingGroupName') || \
+            { cjg_showerror "Can't recover ASG Name containing instance '$myInstanceID'. Aborting"; exit 1; }
 
-readarray instanceids <<< $(aws autoscaling describe-auto-scaling-groups \
-    --auto-scaling-group-name $asgName --output json --region $region \
-    --query 'AutoScalingGroups[*].Instances[?LifecycleState==`InService`]' | \
-    jq -r '.[] | .[] | .InstanceId')
+    #echo "the asg Name is $asgName"
 
-echo "the instance IDs are $instanceids"
+    readarray instanceids <<< $(aws autoscaling describe-auto-scaling-groups \
+        --auto-scaling-group-name $asgName --output json --region $region \
+        --query 'AutoScalingGroups[*].Instances[?LifecycleState==`InService`]' | \
+        jq -rce '.[] | .[] | .InstanceId') || \
+            { cjg_showerror "Can't recover any Instance ID in the ASG '$asgName'. Aborting"; exit 1; }
 
-ipaddresses=$(aws ec2 describe-instances --output json --region $region \
-    --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PrivateIpAddress:PrivateIpAddress}" --instance-id $instanceids | \
-    jq -r '.[] | .[] | .PrivateIpAddress')
+    #echo "the instance IDs are $instanceids"
+
+    ipaddresses=$(aws ec2 describe-instances --output json --region $region \
+        --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PrivateIpAddress:PrivateIpAddress}" --instance-id $instanceids | \
+        jq -rce '.[] | .[] | .PrivateIpAddress') || \
+            { cjg_showerror "Can't recover any Instance IP in the ASG '$asgName' for instances '$instanceids'. Aborting"; exit 1; }
+    
+    echo $ipaddresses
+}
+
+ips=$(get_ips_this_asg) || \
+            { cjg_showerror "Premature End of Script"; exit 1; }
+    
 
 echo "the IPs are:"
 
@@ -43,7 +62,7 @@ echo "the IPs are:"
 
 # List of IP, comma separated
 output=""
-for ipaddress in $ipaddresses
+for ipaddress in $ips
 do
     if [ -z "$output" ]
     then
