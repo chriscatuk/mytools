40ad04a717edf152d989c6d0a6b6103226dbf152
Christian Gonzalvez <christian.gonzalvez@cloudreach.com>
2019-02-02 16:02:28 +0000

    improve AWS CLI commands

--- aws/list-asg-ips.sh
+++ aws/list-asg-ips.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
-# list all instances of the ASG this instance is from
+# list all instances in Service of the ASG this instance is from
 # IPs comma separated
 
-myid=`curl --silent http://169.254.169.254/latest/meta-data/instance-id 2>&1`
+# Check if required commands are installed
+for item in logger \
+            jq \
+            aws \
+            curl
+  do
+    command -v $Item >/dev/null 2>&1 || \
+        { logger -s "DNS Check requires {$item} but it's not installed. Aborting."; exit 1; }
+  done
 
-instanceids=`aws autoscaling describe-auto-scaling-instances \
---output text --region eu-west-1 \
---instance-ids $myid \
---query "AutoScalingInstances[*].AutoScalingGroupName" \
-| xargs -I '{}' aws autoscaling describe-auto-scaling-instances --region eu-west-1 --output text \
---query "AutoScalingInstances[?AutoScalingGroupName=='{}'].[InstanceId]"`
+read instanceID region <<< $(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | \
+    jq -c -r '.instanceId, .region')
 
-ipaddresses=`aws ec2 describe-instances --output text --region eu-west-1 \
---query "Reservations[*].Instances[*].[PrivateIpAddress]" --instance-id $instanceids`
+echo "the instance ID is $instanceID, in region $region"
+
+asgName=$(aws autoscaling describe-auto-scaling-instances \
+    --output json --region $region \
+    --instance-ids $myInstanceID \
+    --query "AutoScalingInstances[0]" | \
+    jq -c -r '.AutoScalingGroupName')
+
+echo "the asg Name is $asgName"
+
+readarray instanceids <<< $(aws autoscaling describe-auto-scaling-groups \
+    --auto-scaling-group-name $asgName --output json --region $region \
+    --query 'AutoScalingGroups[*].Instances[?LifecycleState==`InService`]' | \
+    jq -r '.[] | .[] | .InstanceId')
+
+echo "the instance IDs are $instanceids"
+
+ipaddresses=$(aws ec2 describe-instances --output json --region $region \
+    --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PrivateIpAddress:PrivateIpAddress}" --instance-id $instanceids | \
+    jq -r '.[] | .[] | .PrivateIpAddress')
+
+echo "the IPs are:"
 
 # List of IP
 # echo $ipaddresses
