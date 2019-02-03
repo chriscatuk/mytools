c16c23be4ab7075a187ab0197870212e4a90a93a
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2019-02-03 14:55:03 +0000

    error handling

--- aws/list-asg-ips.sh
+++ aws/list-asg-ips.sh
@@ -21,9 +21,16 @@ for item in logger \
 
 
 function get_ips_this_asg() {
-    read instanceID region <<< $(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | \
-        jq --compact-output --raw-output --exit-status '.instanceId, .region') || \
-            { cjg_showerror "Can't recover Meta-Data Instance ID and Region. Aborting"; exit 1; }
+
+    output=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document)
+    if [ "$?" -ne 0 -o -z "$output" ] ; then
+        cjg_showerror "Can't recover Meta-Data. Aborting"; exit 1;
+    fi
+
+    read instanceID region <<< $(jq --compact-output --raw-output --exit-status '.instanceId, .region' 2> /dev/null <<< $output)
+    if [ "$?" -ne 0 -o -z "$instanceID" -o -z "$region" ] ; then
+        cjg_showerror "Can't extract Instance ID and Region from Meta-Data. Aborting"; exit 1;
+    fi
 
     #echo "the instance ID is $instanceID, in region $region"
 
@@ -31,29 +38,29 @@ function get_ips_this_asg() {
         --output json --region $region \
         --instance-ids $myInstanceID \
         --query "AutoScalingInstances[0]" | \
-        jq -rce '.AutoScalingGroupName') || \
-            { cjg_showerror "Can't recover ASG Name containing instance '$myInstanceID'. Aborting"; exit 1; }
+        jq -rce '.AutoScalingGroupName' || \
+            { cjg_showerror "Can't recover ASG Name containing instance '$myInstanceID'. Aborting"; exit 1; })
 
     #echo "the asg Name is $asgName"
 
     readarray instanceids <<< $(aws autoscaling describe-auto-scaling-groups \
         --auto-scaling-group-name $asgName --output json --region $region \
         --query 'AutoScalingGroups[*].Instances[?LifecycleState==`InService`]' | \
-        jq -rce '.[] | .[] | .InstanceId') || \
-            { cjg_showerror "Can't recover any Instance ID in the ASG '$asgName'. Aborting"; exit 1; }
+        jq -rce '.[] | .[] | .InstanceId' || \
+            { cjg_showerror "Can't recover any Instance ID in the ASG '$asgName'. Aborting"; exit 1; })
 
     #echo "the instance IDs are $instanceids"
 
     ipaddresses=$(aws ec2 describe-instances --output json --region $region \
         --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PrivateIpAddress:PrivateIpAddress}" --instance-id $instanceids | \
-        jq -rce '.[] | .[] | .PrivateIpAddress') || \
-            { cjg_showerror "Can't recover any Instance IP in the ASG '$asgName' for instances '$instanceids'. Aborting"; exit 1; }
+        jq -rce '.[] | .[] | .PrivateIpAddress' || \
+            { cjg_showerror "Can't recover any Instance IP in the ASG '$asgName' for instances '$instanceids'. Aborting"; exit 1; })
     
     echo $ipaddresses
 }
 
-ips=$(get_ips_this_asg) || \
-            { cjg_showerror "Premature End of Script"; exit 1; }
+ips=$(get_ips_this_asg || \
+            { cjg_showerror "Premature End of Script"; exit 1; })
     
 
 echo "the IPs are:"
