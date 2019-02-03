00b7a1f94c95c9790dee28a17a5d7861d0409095
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2019-02-03 18:05:47 +0000

    better error handling

--- aws/list-asg-ips.sh
+++ aws/list-asg-ips.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # list all instances in Service of the ASG this instance is from
 # IPs comma separated
+# use 'jq -rce' for 'jq --raw-output --compact-output --exit-status'
 
 # Output the message to standard error as well as to the system log.
-function cjg_showerror() {
-    logger -s "Error DNS Check: $1"
+function cjg_logerror() {
+    logger -s "[DNS Check][Error] $1" -p user.err
 }
-export -f cjg_showerror
+function cjg_logdebug() {
+    logger -s "[DNS Check][Debug] $1" -p user.debug
+}
+
+echo ""
+echo "******************************************"
+echo "************ START DNS Check *************"
+echo "******************************************"
+echo ""
 
 # Pre Req Check
     # Check if required commands are installed
@@ -16,51 +25,95 @@ for item in logger \
                 curl
     do
         command -v "$item" > /dev/null || \
-            { cjg_showerror "requires {$item} but it's not installed. Aborting."; exit 1; }
+            { cjg_logerror "requires {$item} but it's not installed. Aborting."; exit 1; }
     done
 
 
 function get_ips_this_asg() {
 
-    output=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document)
-    if [ "$?" -ne 0 -o -z "$output" ] ; then
-        cjg_showerror "Can't recover Meta-Data. Aborting"; exit 1;
+    # Instance ID + Region
+    curlcommand="curl -s http://169.254.169.254/latest/dynamic/instance-identity/document"
+    output=$($curlcommand)
+    if [ "$?" -ne 0 ] || [ -z "$output" ] ; then
+        cjg_logerror "Can't recover Meta-Data. Aborting"; exit 1;
     fi
 
-    read instanceID region <<< $(jq --compact-output --raw-output --exit-status '.instanceId, .region' 2> /dev/null <<< $output)
-    if [ "$?" -ne 0 -o -z "$instanceID" -o -z "$region" ] ; then
-        cjg_showerror "Can't extract Instance ID and Region from Meta-Data. Aborting"; exit 1;
+    region=$(jq -rce '.region' 2> /dev/null <<< $output)
+    if [ "$?" -ne 0 ] || [ -z "$region" ] ; then
+        cjg_logerror "Can't extract Region from Meta-Data. Aborting"; exit 1;
     fi
 
-    #echo "the instance ID is $instanceID, in region $region"
+    instanceID=$(jq -rce '.instanceId' 2> /dev/null <<< $output)
+    if [ "$?" -ne 0 ] || [ -z "$instanceID" ] ; then
+        cjg_logerror "Can't extract Instance ID afrom Meta-Data. Aborting"; exit 1;
+    fi
 
-    asgName=$(aws autoscaling describe-auto-scaling-instances \
-        --output json --region $region \
-        --instance-ids $myInstanceID \
-        --query "AutoScalingInstances[0]" | \
-        jq -rce '.AutoScalingGroupName' || \
-            { cjg_showerror "Can't recover ASG Name containing instance '$myInstanceID'. Aborting"; exit 1; })
+    cjg_logdebug "Meta Data Extraction finished, the instance ID is $instanceID, in region $region"
 
-    #echo "the asg Name is $asgName"
+    # ASG Name
+    awscommand="aws autoscaling describe-auto-scaling-instances --output json --region $region --instance-ids $instanceID"
+    output=$($awscommand)
 
-    readarray instanceids <<< $(aws autoscaling describe-auto-scaling-groups \
+    if [ "$?" -ne 0 ] || [ -z "$output" ] ; then
+        cjg_logerror "Can't recover ASG Name containing instance '$instanceID'. Aborting"
+        cjg_logdebug "AWS CLI: $awscommand"
+        cjg_logdebug "AWS Result: $output"
+        exit 1
+    fi
+
+    asgName=$(jq -rce '.AutoScalingInstances[0] | .AutoScalingGroupName' 2> /dev/null <<< $output)
+    if [ "$?" -ne 0 ] || [ -z "$asgName" ] ; then
+        cjg_logerror "Can't extract ASG Name containing instance '$instanceID'. Aborting";
+        cjg_logdebug "the asg Name is $asgName"
+        cjg_logdebug "AWS CLI: $awscommand"
+        cjg_logdebug "AWS Result: $output"
+        exit 1;
+    fi
+
+    cjg_logdebug "ASG Identification finished, the ASG Name is $asgName"
+
+    #cjg_logdebug "the asg Name is $asgName"
+
+    # readarray instanceids <<< $(aws autoscaling describe-auto-scaling-groups \
+    #     --auto-scaling-group-name $asgName --output json --region $region \
+    #     --query 'AutoScalingGroups[*].Instances[?LifecycleState==`InService`]' | \
+    #     jq -rce '.[] | .[] | .InstanceId' || \
+    #         { cjg_logerror "Can't recover any Instance ID in the ASG '$asgName'. Aborting"; exit 1; })
+
+    # Instances inServcie in ASG
+    output=$(aws autoscaling describe-auto-scaling-groups \
         --auto-scaling-group-name $asgName --output json --region $region \
-        --query 'AutoScalingGroups[*].Instances[?LifecycleState==`InService`]' | \
-        jq -rce '.[] | .[] | .InstanceId' || \
-            { cjg_showerror "Can't recover any Instance ID in the ASG '$asgName'. Aborting"; exit 1; })
+        --query 'AutoScalingGroups[*].Instances[?LifecycleState==`InService`]')
 
-    #echo "the instance IDs are $instanceids"
+    if [ "$?" -ne 0 ] || [ -z "$output" ] ; then
+        cjg_logerror "Can't recover any Instance ID in Service in the ASG '$asgName'. Aborting"
+        exit 1
+    fi
+
+    instanceids=$(jq -rce '.[] | .[] | .InstanceId' 2> /dev/null <<< $output)
+    if [ "$?" -ne 0 ] || [ -z "$instanceids" ] ; then
+        cjg_logerror "Can't extract any Instance ID in Service in the ASG '$asgName'. Aborting";
+        cjg_logdebug "the instance ids are $instanceids"
+        cjg_logdebug "AWS CLI: $awscommand"
+        cjg_logdebug "AWS Result: $output"
+        exit 1;
+    fi
+
+    #cjg_logdebug "the instance IDs are $instanceids"
 
     ipaddresses=$(aws ec2 describe-instances --output json --region $region \
         --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PrivateIpAddress:PrivateIpAddress}" --instance-id $instanceids | \
         jq -rce '.[] | .[] | .PrivateIpAddress' || \
-            { cjg_showerror "Can't recover any Instance IP in the ASG '$asgName' for instances '$instanceids'. Aborting"; exit 1; })
+            { cjg_logerror "Can't recover any Instance IP in the ASG '$asgName' for instances '$instanceids'. Aborting"; exit 1; })
     
     echo $ipaddresses
 }
 
-ips=$(get_ips_this_asg || \
-            { cjg_showerror "Premature End of Script"; exit 1; })
+ips=$(get_ips_this_asg)
+
+if [ "$?" -ne 0 ] ; then
+    cjg_logerror "Premature End of Script"; exit 1;
+fi
     
 
 echo "the IPs are:"
@@ -80,3 +133,4 @@ do
     fi
 done
 echo $output
+
