41753cd8884fa7952e1c92fe3858c1be882491ec
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2021-04-06 20:50:18 +0100

    Revert "Bucket module (#2)" (#4)
    This reverts commit e41021e8d16519037809676e51281d4b53aa2842.

--- aws/terraform-module-bucket/modules/bucket/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">=0.13"
-  required_providers {
-    aws = {
-      version = ">= 3.26"
-    }
-  }
-}
--- aws/terraform-module-bucket/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "buckets" {
-  description = "Return all the outputs from the module"
-  value       = { for bucket in module.buckets : bucket.id => bucket.arn }
-}
